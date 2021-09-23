f580ae8541a15d10fdba0c26dc75db7ee1f68763
ddosul-it <kids83@naver.com>
2021-09-23 17:02:23 +0900

    public

--- app/app.js
+++ app/app.js
@@ -12,9 +12,9 @@ const home = require("./src/routes/home");
 //앱 셋팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
-//미들웨어 접속 use -> 미들웨어를 등록해 주는 메서드
-app.use("/", home);
+app.use("/", home); //미들웨어 접속 use -> 미들웨어를 등록해 주는 메서드
 
    
 module.exports = app;
---
+++ app/src/public/js/home/login.js
@@ -0,0 +1,5 @@
+//이크마스크립트 문법을 준수 하겠다라는 내용
+"use strict"; 
+
+
+console.log("hello"); 

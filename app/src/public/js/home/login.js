7104b4e9a87a3199d9b97dc2c25caf0d03e758c0
ddosul-it <kids83@naver.com>
2021-09-23 17:40:33 +0900

    dom 객체 설정

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,23 @@
 //이크마스크립트 문법을 준수 하겠다라는 내용
 "use strict"; 
 
+//DOM ->Document Object Model
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
 
-console.log("hello"); 
+loginBtn.addEventListener("click", login)
+
+function login (){
+    const req = {
+        id : id.value,
+        psword : psword.value,
+    };
+    console.log(req);
+}
+
+    //fetch();
+
+    // console.log(id); 
+    // console.log("hello");  
+    

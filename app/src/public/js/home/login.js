a2e438be5241222714ed3b7edd1d68ef78f7b1f8
ddosul-it <kids83@naver.com>
2021-09-24 11:17:14 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,15 +18,24 @@ function login (){
     // console.log(req, JSON.stringify(req));
 
     fetch("/login", {
-        method: "POST" ,
-        
-        headers : {
-            "Content-Type" : "application/json",
-        },
-        body: JSON.stringify(req),
+      method: "POST" ,        
+      headers : {
+         "Content-Type" : "application/json",
+      },
+      body: JSON.stringify(req),
     })
-        .then((res) => res.json())
-        .then( console.log);
+      .then((res) => res.json())
+      .then((res) => {
+        if(res.success){
+                location.href = "/";
+        }
+        else{
+                alert(res.msg);
+        }
+     })
+     .catch((err) => {
+        console.error(new Error("로그인 중 에러 발생"));
+     });
 
 }
 
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -10,7 +10,7 @@ const ctrl = require("./home.ctrl");
 router.get("/", ctrl.output.home);
 //app.get("/login", (req,res) => {
 router.get("/login", ctrl.output.login);
-router.post("/login", ctrl.process.login);
+router.post("/login", ctrl.process.login); 
 
 
-module.exports = router ;
+module.exports = router ; 

28a7683d0fdae9d5ceff1a3b70a8a8cd3bd1dace
ddosul-it <kids83@naver.com>
2021-09-24 11:06:32 +0900

    로그인기능정의

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,12 @@
 //이크마스크립트 문법을 준수 하겠다라는 내용
 "use strict"; 
 
+const users= {
+    id : ["kids83", "홍길동", "김팀장"],
+    psword : ["1234","1234","123456"],
+}
+
+
 const output = {
 
     home : (req,res) => {
@@ -17,8 +23,26 @@ const output = {
 
 const process = {
     login : (req,res) => {
-        console.log(req,body);
-    }
+        const id = req.body.id,
+          psword = req.body.psword;
+
+
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword) {
+                return res.json({
+                    success:true,
+                });
+            }
+
+        }  
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패 하셨습니다.",
+        });
+
+    },
 }
 
 
